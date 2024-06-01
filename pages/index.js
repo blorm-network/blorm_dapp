0f8b86d837f1f6e882a96c6689f49ce7474e672c
yu-jeffy <105130385+yu-jeffy@users.noreply.github.com>
2024-05-31 17:15:47 -0700

    style navbar

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,30 @@
-// pages/index.js
-
 import SwapForm from '../components/SwapForm';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <header className={styles.header}>
-        blorm
-      </header>
-      <main className={styles.main}>
+      <div className={styles.header}>
+        <img
+          className={styles.logo}
+          src="blorm-logo-white.png"
+          alt="blorm logo"
+        />
+        <span>blorm</span>
+      </div>
+      <div className={styles.main}>
         <h1 className={styles.mainHeading}>Introducing Blorm</h1>
         <h2 className={styles.subHeading}>Decentralized Asset Courier </h2>
         <p className={styles.description}>
-         Blorm facilitates seamless token transfers across 50+ chains.</p>
+          Blorm facilitates seamless token transfers across 50+ chains.
+        </p>
         <SwapForm />
         <h1 className={styles.mainHeading}>More Text</h1>
         <h2 className={styles.subHeading}>Here </h2>
         <p className={styles.description}>
-         More text here</p>
-      </main>
+          More text here
+        </p>
+      </div>
     </div>
   );
 }
