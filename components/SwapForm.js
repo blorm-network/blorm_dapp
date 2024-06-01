d44237819ada1dacdb30a7e55a45bf8b7e6b10a7
yu-jeffy <105130385+yu-jeffy@users.noreply.github.com>
2024-06-01 16:56:45 -0700

    fetch chains and assets from skip api, populate dropdowns from network -> supported tokens

--- components/SwapForm.js
+++ components/SwapForm.js
@@ -1,11 +1,10 @@
 import { useState, useEffect } from 'react';
-import { assets as allAssets, chains as allChains } from 'chain-registry';
-import { chainRegistryChainToKeplr } from '@chain-registry/keplr';
 import Select from 'react-select';
+import { SigningCosmosClient } from "@cosmjs/launchpad";
+import { SkipRouter } from "@skip-router/core";
 import styles from '../styles/SwapForm.module.css';
 
 export default function SwapForm() {
-  // State variables
   const [keplr, setKeplr] = useState(null);
   const [sourceAssetDenom, setSourceAssetDenom] = useState(null);
   const [sourceAssetChainID, setSourceAssetChainID] = useState(null);
@@ -16,102 +15,117 @@ export default function SwapForm() {
   const [addresses, setAddresses] = useState({});
   const [connected, setConnected] = useState(false);
   const [assets, setAssets] = useState([]);
-  const [chains, setChains] = useState(allChains);
+  const [chains, setChains] = useState([]);
+  const [isRawOutputVisible, setIsRawOutputVisible] = useState(false);
+  const [assetOptionsSource, setAssetOptionsSource] = useState([]);
+  const [assetOptionsDest, setAssetOptionsDest] = useState([]);
+  const [chainOptions, setChainOptions] = useState([]);
+
 
   useEffect(() => {
-    // Fetch and filter assets
-    const filteredAssets = allAssets.reduce((acc, asset) => {
-      return acc.concat(asset.assets);
-    }, []);
+    const fetchChains = async () => {
+      const skipClient = new SkipRouter();
+      const resultChains = await skipClient.chains();
+      console.log("Fetched Chains:", resultChains);
+      setChains(resultChains);
+    };
+    fetchChains();
 
-    // Filter unique assets based on their symbol
-    const uniqueAssets = Array.from(new Set(filteredAssets.map(a => a.symbol)))
-      .map(symbol => {
-        return filteredAssets.find(a => a.symbol === symbol);
+    const fetchAssets = async () => {
+      const res = await fetch('/api/ibc-chains', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
       });
+      const data = await res.json();
+      console.log("Fetched Assets:", data);
+      setAssets(data);
+    };
+    fetchAssets();
+  }, []);
+  
+  useEffect(() => {
+    if (chains) {
+      setChainOptions(chains.map(chain => ({
+        value: chain.chainID,
+        label: chain.chainID + " / " + chain.chainName,
+      })));
+    }
+  }, [chains]);
 
-    // Set assets state
-    setAssets(filteredAssets);
+  useEffect(() => {
+    if (sourceAssetChainID && assets && sourceAssetChainID.value in assets.chain_to_assets_map) {
+      setAssetOptionsSource(assets.chain_to_assets_map[sourceAssetChainID.value].assets.map(asset => ({
+        value: asset.denom,
+        label: asset.name + " (" + asset.denom + " / " + asset.symbol + " / " + asset.origin_denom + ")",
+      })));
+      console.log("Source Asset Options set:", assetOptionsSource);
+    }
+  }, [sourceAssetChainID]);
+
+  useEffect(() => {
+    if (destAssetChainID && assets && destAssetChainID.value in assets.chain_to_assets_map) {
+      setAssetOptionsDest(assets.chain_to_assets_map[destAssetChainID.value].assets.map(asset => ({
+        value: asset.denom,
+        label: asset.name + " (" + asset.denom + " / " + asset.symbol + " / " + asset.origin_denom + ")",
+      })));
+      console.log("Source Asset Options set:", assetOptionsSource);
+    }
+  }, [destAssetChainID]);
+
+  useEffect(() => {
+    const loadKeplr = async () => {
+      if (typeof window !== 'undefined') {
+        while (!window.keplr) {
+          await new Promise(resolve => setTimeout(resolve, 100));
+        }
+        setKeplr(window.keplr);
+      }
+    };
+    loadKeplr();
+    window.addEventListener("keplr_keystorechange", () => {
+      console.log("Key store in Keplr is changed. You may need to refetch the account info.");
+    });
   }, []);
 
-  useEffect(() => {
-    // Update chains state based on selected source asset
-    // Check if a source asset is selected
-    if (sourceAssetDenom) {
-      // Find the selected asset from the assets array
-      const selectedAsset = assets.find(a => a.base === sourceAssetDenom.value);
-      // If the selected asset is found
-      if (selectedAsset) {
-        // Filter the chains array to include only the chains with the same chain_name as the selected asset
-        const assetChains = allChains.filter(chain => chain.name === selectedAsset.chain_name);
-        // Update the chains state with the filtered chains
-        setChains(assetChains);
-      }
-    } else {
-      // If no source asset is selected, set the chains state to include all chains
-      setChains(allChains);
-    }
-  }, [sourceAssetDenom, assets]);
-
   const connectKeplr = async () => {
-    // Connect to Keplr wallet
-    if (keplr) {
-      const selectedChains = [
-        chains.find(chain => chain.chain_id === sourceAssetChainID?.value),
-        chains.find(chain => chain.chain_id === destAssetChainID?.value)
-      ].filter(Boolean);
-
-      if (selectedChains.length === 2) {
-        // Suggest and enable selected chains
-        await Promise.all(selectedChains.map(async (chain) => {
-          const chainConfig = chainRegistryChainToKeplr(chain, allAssets);
-          await keplr.experimentalSuggestChain(chainConfig);
-        }));
-
-        await keplr.enable(selectedChains.map(chain => chain.chain_id));
-
-        // Get account info for selected chains
-        const accountInfo = await Promise.all(
-          selectedChains.map(async (chain) => {
-            const key = await keplr.getKey(chain.chain_id);
-            return { chainId: chain.chain_id, address: key.bech32Address };
-          })
-        );
-
-        // Set addresses state and mark as connected
-        const addressMap = accountInfo.reduce((acc, info) => {
-          acc[info.chainId] = info.address;
-          return acc;
-        }, {});
-
-        setAddresses(addressMap);
-        setConnected(true);
-      } else {
-        alert('Please select both source and destination chains.');
+    if (!keplr) {
+      alert("Please install Keplr extension");
+      return;
+    }
+    try {
+      const chainIds = route?.requiredChainAddresses || [sourceAssetChainID?.value, destAssetChainID?.value].filter(Boolean);
+      if (chainIds.length < 2) {
+        alert("Please select both source and destination chains or find a route first.");
+        return;
       }
-    } else {
-      alert('Please install Keplr extension.');
+      await keplr.enable(chainIds);
+      const addressMap = {};
+      for (let chainId of chainIds) {
+        const offlineSigner = keplr.getOfflineSigner(chainId);
+        if (!offlineSigner) {
+          alert(`Failed to get offline signer for chain ${chainId}`);
+          return;
+        }
+        const accounts = await offlineSigner.getAccounts();
+        addressMap[chainId] = accounts[0].address;
+      }
+      setAddresses(addressMap);
+      setConnected(true);
+    } catch (error) {
+      console.error(error);
+      alert("Failed to connect to Keplr");
     }
   };
 
   const handleFindRoute = async (e) => {
     e.preventDefault();
-
-    // Validate form fields
-    if (
-      !sourceAssetDenom ||
-      !sourceAssetChainID ||
-      !destAssetDenom ||
-      !destAssetChainID ||
-      !amountIn
-    ) {
+    if (!sourceAssetDenom || !sourceAssetChainID || !destAssetDenom || !destAssetChainID || !amountIn) {
       alert('Please fill in all fields.');
       return;
     }
-
-    console.log('Finding route with these details:', {sourceAssetDenom, sourceAssetChainID, destAssetDenom, destAssetChainID, amountIn})
-
-    // Prepare request body
+    console.log('Finding route with these details:', { sourceAssetDenom, sourceAssetChainID, destAssetDenom, destAssetChainID, amountIn });
     const requestBody = {
       sourceAssetDenom: sourceAssetDenom.value,
       sourceAssetChainID: sourceAssetChainID.value,
@@ -120,69 +134,94 @@ export default function SwapForm() {
       amountIn,
       smartSwapOptions: { splitRoutes: true },
     };
-  
-    // Send request to find route
-    const res = await fetch('/api/find-route', {
+    const res = await fetch('/api/ibc-route', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(requestBody),
     });
-  
     if (res.ok) {
       const data = await res.json();
       setRoute(data);
-      alert('Route found successfully!');
     } else {
       const errorData = await res.json();
-      alert(`Error finding route: ${errorData.error}`);
-    }
-  };  
-
-  const handleExecuteRoute = async () => {
-    // Execute route
-    const res = await fetch('/api/execute-route', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ route, addresses }),
-    });
-
-    if (res.ok) {
-      alert('Route executed successfully!');
-    } else {
-      alert('Error executing route');
+      alert(`Error finding route: ${errorData}`);
     }
   };
 
-  // Prepare options for asset select
-  const assetOptions = assets.map(asset => ({
-    value: asset.base,
-    label: asset.display + " (" + asset.symbol + ", " + asset.name + ", " + asset.base + ")",
-  }));
+  const handleExecuteRoute = async () => {
+    if (!route) {
+      alert('Please find a route first.');
+      return;
+    }
+    if (!keplr) {
+      alert("Please install Keplr extension");
+      return;
+    }
+    try {
+      const chainIds = route.requiredChainAddresses;
+      await keplr.enable(chainIds);
 
-  // Prepare options for chain select
-  const chainOptions = chains.map(chain => ({
-    value: chain.chain_id,
-    label: chain.chain_id + " / " + chain.chain_name,
-  }));
+      const addressMap = {};
+      const signers = {};
+      for (let chainId of chainIds) {
+        const offlineSigner = keplr.getOfflineSigner(chainId);
+        const accounts = await offlineSigner.getAccounts();
+        addressMap[chainId] = accounts[0].address;
+        signers[chainId] = offlineSigner; // Store the offline signer
+      }
+
+      console.log(addressMap, signers);
+
+      const cosmosSigners = Object.entries(signers).reduce((acc, [chainID, signer]) => {
+        acc[chainID] = new SigningCosmosClient("https://lcd-cosmoshub.keplr.app", addresses[chainID], signer);
+        return acc;
+      }, {});
+
+      // Initialize SkipRouter with the signers
+      const skipClient = new SkipRouter({ getCosmosSigner: (chainID) => cosmosSigners[chainID] });
+
+      await skipClient.executeRoute({
+        route,
+        userAddresses: Object.entries(addresses).map(([chainID, address]) => ({ chainID, address })),
+        onTransactionCompleted: (chainID, txHash, status) => {
+          console.log(`Route completed with tx hash: ${txHash} & status: ${status.state}`);
+        },
+      });
+
+      alert('Route executed successfully!');
+    } catch (error) {
+      console.error('Error executing route:', error);
+      // alert('Error executing route');
+    }
+  };
+
+  const generateReadableRoute = (route) => {
+    if (!route || !route.operations) return '';
+    const truncateDenom = (denom) => {
+      if (denom.length > 12) {
+        return denom.slice(0, 7) + '...' + denom.slice(-5);
+      }
+      return denom;
+    };
+    const routeParts = route.operations.map((operation, index) => {
+      const type = Object.keys(operation)[0];
+      if (type === 'transfer') {
+        const transfer = operation.transfer;
+        return `Transfer ${truncateDenom(transfer.denomIn)} from ${transfer.fromChainID} to ${transfer.toChainID}`;
+      } else if (type === 'swap') {
+        const swap = operation.swap;
+        return `Swap ${truncateDenom(swap.denomIn)} on ${swap.swapIn.swapVenue.name}`;
+      }
+    });
+    return routeParts.join(' -> ');
+  };
 
   return (
     <div className={styles.container}>
       <form onSubmit={handleFindRoute} className={styles.form}>
-        <div className={styles.formGroup}>
-          <label>Source Token:</label>
-          <Select
-            className="w-full"
-            value={sourceAssetDenom}
-            onChange={setSourceAssetDenom}
-            options={assetOptions}
-            placeholder="Search for a token ..."
-          />
-        </div>
-        <div className={styles.formGroup}>
+      <div className={styles.formGroup}>
           <label>Source Network:</label>
           <Select
             className="w-full"
@@ -190,16 +229,15 @@ export default function SwapForm() {
             onChange={setSourceAssetChainID}
             options={chainOptions}
             placeholder="Search for a network ..."
-            isDisabled={!sourceAssetDenom}
           />
         </div>
         <div className={styles.formGroup}>
-          <label>Destination Token:</label>
+          <label>Source Token:</label>
           <Select
             className="w-full"
-            value={destAssetDenom}
-            onChange={setDestAssetDenom}
-            options={assetOptions}
+            value={sourceAssetDenom}
+            onChange={setSourceAssetDenom}
+            options={assetOptionsSource}
             placeholder="Search for a token ..."
           />
         </div>
@@ -211,29 +249,54 @@ export default function SwapForm() {
             onChange={setDestAssetChainID}
             options={chainOptions}
             placeholder="Search for a network ..."
-            isDisabled={!sourceAssetDenom}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label>Destination Token:</label>
+          <Select
+            className="w-full"
+            value={destAssetDenom}
+            onChange={setDestAssetDenom}
+            options={assetOptionsDest}
+            placeholder="Search for a token ..."
           />
         </div>
         <div className={styles.formGroup}>
           <label>Amount In:</label>
-        <input
-          type="text"
-          value={amountIn}
-          placeholder="1000000"
-          onChange={(e) => setAmountIn(e.target.value)}
-          className="w-full border border-gray-300 rounded p-2"
-          defaultValue={1000000}
-        />
+          <input
+            type="text"
+            value={amountIn}
+            placeholder="1000000"
+            onChange={(e) => setAmountIn(e.target.value)}
+            className="w-full border border-gray-300 rounded p-2"
+          />
         </div>
         <button type="submit">Find Route</button>
       </form>
       {route && (
         <div className={styles.results}>
+          <h1>We found a route!</h1>
+          <br />
           <h2>Route Details</h2>
-          <pre>{JSON.stringify(route, null, 2)}</pre>
+          <p>{generateReadableRoute(route)}</p>
+          <button onClick={() => setIsRawOutputVisible(!isRawOutputVisible)}>
+            {isRawOutputVisible ? 'Hide' : 'Show'} Raw Output
+          </button>
+          {isRawOutputVisible && (
+            <pre>{JSON.stringify(route, null, 2)}</pre>
+          )}
           <button onClick={handleExecuteRoute}>Execute Route</button>
         </div>
       )}
+      <div className={styles.keplr}>
+        <button onClick={connectKeplr} className="text-white">Connect Keplr</button>
+        {connected && (
+          <div>
+            <h2 className="text-white">Connected Addresses</h2>
+            <pre className="text-white">{JSON.stringify(addresses, null, 2)}</pre>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ pages/api/ibc-chains.js
@@ -0,0 +1,16 @@
+// pages/api/ibc-chains.js
+import sdk from '@api/skip-ibc-api';
+
+export default async function handler(req, res) {
+    if (req.method === 'GET') {
+        try {
+            const data = await sdk.getAssets();
+            console.log("ibc-chains API called, data: ", data.data);
+            res.status(200).json(data.data);
+        } catch (error) {
+            res.status(500).json({ error: 'Error fetching chains' });
+        }
+    } else {
+        res.status(405).json({ error: 'Method not allowed' });
+    }
+}
